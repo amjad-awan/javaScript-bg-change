cda5906c7f99432993c61d1ddab0b995376d7bc9
amjad-awan <73718759+amjad-awan@users.noreply.github.com>
2021-08-16 10:19:15 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 //alert(getColor.length);
 let colorChange = document.querySelector(".color-change");
 const colorClick = document.querySelectorAll(".click-color");
-console.log(colorClick);
+
 
 colorClick.forEach((element,index) => {
   element.addEventListener("click",function(){
     let innerColor= colorClick[index].innerHTML;
-    console.log(innerColor);
+    
 
     colorChange.style.backgroundColor=innerColor;
   }) 
