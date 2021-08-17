6d4c91d9b9bc41d64c41fb68862847c366d9988a
amjad-awan <73718759+amjad-awan@users.noreply.github.com>
2021-08-17 00:45:55 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -4,13 +4,34 @@ let colorChange = document.querySelector(".color-change");
 const colorClick = document.querySelectorAll(".click-color");
 
 
-colorClick.forEach((element,index) => {
-  element.addEventListener("click",function(){
-    let innerColor= colorClick[index].innerHTML;
-    
 
+colorClick.forEach((element,index) =>{
+  element.addEventListener("click",function(){
+    let innerColor= colorClick[index].innerText;
+    element.classList.add("dorotate");
     colorChange.style.backgroundColor=innerColor;
+
+    setTimeout(()=>{
+      element.classList.remove("dorotate");
+      
+    let getFirstChild = element.firstChild;
+    console.log(getFirstChild);
+    //let getAudio= colorClick[index].innerHTML;
+    getFirstChild.play();
+    //console.log(getAudio);
+    },300);
+
+
+    
   }) 
+
+  /*element.addEventListener("mouseout",function(){
+    //let innerColor= colorClick[index].innerHTML;
+    element.classList.remove("dorotate");
+    //colorChange.style.backgroundColor=innerColor;
+  })*/
+
+ 
 });
 
 
